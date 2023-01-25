c5d910eae7aaa5e14d6593cb9414911c283b632e
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-01-25 18:17:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import readlineSync from "readline-sync"
 import chalk from "chalk";
-const properties = [];
+const propriedade = [];
 let input = "";
 
 console.log("Digite as propriedades CSS. Digite 'SAIR' para finalizar.");
@@ -8,9 +8,9 @@ console.log("Digite as propriedades CSS. Digite 'SAIR' para finalizar.");
 while (input.toLowerCase() !== "sair") {
     input = readlineSync.question("Digite uma propriedade: ");
     if (input.toLowerCase() !== "sair") {
-        properties.push(input);
+        propriedade.push(input);
     }
 }
 
 console.log("Lista ordenada de propriedades CSS:")
-console.log(chalk.red.underline(properties.sort().join("\n")));
+console.log(chalk.red.underline(propriedade.sort().join("\n")));
